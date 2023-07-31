ca6d5a1659d8966213bcd140b0a3988f04eefb3a
Achintha2722 <135610167+Achintha2722@users.noreply.github.com>
2023-07-31 20:57:25 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+
+
+//Q2
+
+void cal(int a,int b)
+{
+    int sum,diff;
+    sum=a+b;
+    diff=a-b;
+    printf("sum = %d\n",sum);
+    printf("difference = %d \n",diff);
+}
+
+int main()
+{
+    int x,y;
+
+    printf("Enter two numbers:");
+    scanf("%d%d",&x,&y);
+
+    cal(x,y);
+}
+
+
+
+
+//Q1
+void cal()
+{
+    int n1,n2,sum;
+
+    printf("Enter two numbers:");
+    scanf("%d%d",&n1,&n2);
+
+    (sum=n1+n2);
+    printf("sum = %d ",sum);
+}
+
+int main()
+{
+   cal();
+}
